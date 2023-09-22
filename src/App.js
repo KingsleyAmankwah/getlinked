f7083fdd37c54b4827a7b9cb351f8271b541ee5c
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-09-22 19:12:48 +0000

    App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Hero from "./components/Hero";
-import Nav from "./components/Nav";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-export default function App() {
+import Contact from "./pages/Contact";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
+function App() {
   return (
-    <div className="w-full h-full bg-color">
-      <Nav />
-      <Hero />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+export default App;
